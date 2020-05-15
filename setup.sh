9af875aba90dbbef60c06f7b83d6dc59f1a75b25
tserg <tse.rong.gary@gmail.com>
2020-05-15 23:29:41 +0800

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+export FLASK_APP=app.py
+export AUTH0_DOMAIN=fsndgt.au.auth0.com
+export ALGORITHMS=['RS256']
+export API_AUDIENCE=dev
+export DATABASE_PATH=postgres://zdxmkazppjwplp:c8de773965b2868c668bb4a8c51e24fc4f86109c0e0bc9d099a454b988d2bdb8@ec2-3-222-150-253.compute-1.amazonaws.com:5432/d54jmkode95irn
+export CLIENT_ID=k0UiQ1k69kaVYwB7Os4lep8kHQRTj1do
+export CLIENT_SECRET=VJ5B_JG6hYD3YRpp0Ou8EwtA7N6uAr5rBEvHFfFHCuN0XVMfmnqXAe_2h2sLF7v7
