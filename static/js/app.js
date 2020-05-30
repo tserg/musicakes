0cde79a717cabe8e619c77289e783d54e70f1d6d
tserg <tse.rong.gary@gmail.com>
2020-05-30 18:55:10 +0800

    feat: display eth balance
    Display ETH balance of current exposed account
    in Metamask using web3 in app.js.

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,14 @@
 const ethereumButton = document.querySelector('#enableEthereumButton');
-const showAccount = document.querySelector('#account_address');
+const showAccountAddress = document.querySelector('#account_address');
+const showAccountBalance = document.querySelector('#account_balance');
 
 console.log(ethereumButton);
-console.log(showAccount);
+console.log(showAccountAddress);
+console.log(showAccountBalance);
+
+var web3 = new Web3(Web3.givenProvider);
+
+console.log(web3);
 
 ethereumButton.addEventListener('click', () => {
 	getAccount();
@@ -13,5 +19,23 @@ async function getAccount() {
 	console.log(accounts);
 	const account = accounts[0];
 	console.log(account)
-	showAccount.innerHTML = account;
+	showAccountAddress.innerHTML = account;
+
+	var accountWeiBalance = web3.eth.getBalance(account, function(error, result) {
+		if (!error) {
+
+
+		console.log(result);
+		var accountBalance = web3.utils.fromWei(result, "ether");
+		showAccountBalance.innerHTML = accountBalance;
+		} else {
+			console.log(error);
+		}
+	});
+
 }
+
+/* Payment token contract */
+
+
+
