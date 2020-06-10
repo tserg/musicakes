a78bfd0e0b41b4bc1920fe0a54a1cb68954dc259
tserg <tse.rong.gary@gmail.com>
2020-06-10 21:12:37 +0800

    feat: moved auth functions to app.py
    Moved authentication functions in auth.py to app.py
    as token is currently stored in Flask session instead
    of request in browser.
    
    To fix form validation for create artist.

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from forms import *
 from jose import jwt
 
 from models import setup_db, User, Artist, Release, Track, Purchase
-from auth import AuthError, requires_auth, check_auth_id
 
+from urllib.request import urlopen
 from authlib.integrations.flask_client import OAuth
 from six.moves.urllib.parse import urlencode
 
@@ -22,6 +22,10 @@ AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN', 'Does not exist')
 ALGORITHMS = os.getenv('ALGORITHMS', 'Does not exist')
 API_AUDIENCE = os.getenv('API_AUDIENCE', 'Does not exist')
 
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
 
 def create_app(test_config=None):
 
@@ -50,6 +54,199 @@ def create_app(test_config=None):
         },
     )
 
+    # Auth Header
+
+    def get_token_auth_header():
+        auth = request.headers.get('Authorization', None)
+        if not auth:
+            raise AuthError({
+                'code': 'authorization_header_missing',
+                'description': 'Authorization header is expected.'
+            }, 401)
+
+        parts = auth.split()
+        if parts[0].lower() != 'bearer':
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Authorization header must start with "Bearer".'
+            }, 401)
+
+        elif len(parts) == 1:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Token not found.'
+            }, 401)
+
+        elif len(parts) > 2:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Authorization header must be bearer token.'
+            }, 401)
+
+        token = parts[1]
+        return token
+
+
+    '''
+        @INPUTS
+            permission: string permission (i.e. 'post:drink')
+            payload: decoded jwt payload
+
+        raises an AuthError if permissions are not included in the payload
+        raises an AuthError if the requested permission string is not
+        in the payload permissions array
+        return true otherwise
+    '''
+
+
+    def check_permissions(permission, payload):
+
+        if 'permissions' not in payload:
+            raise AuthError({
+                'code': 'permissions_header_missing',
+                'description': 'Permission header missing'
+            }, 400)
+
+        if permission not in payload['permissions']:
+            raise AuthError({
+                'code': 'no_permission',
+                'description': 'No permission'
+            }, 401)
+
+        return True
+
+    '''
+        @INPUTS
+            auth_id: string auth_id that is unique identifier of user
+            payload: decoded jwt payload
+
+        raises an AuthError if sub is not included in the payload
+        raises an AuthError if the auth_id is not
+        in the payload sub array
+        return true otherwise
+    '''
+
+    def check_auth_id(auth_id, payload):
+
+        if 'sub' not in payload: 
+            raise AuthError({
+                'code': 'sub_header_missing',
+                'description': 'Sub header missing'
+            }, 400)
+
+
+        if auth_id not in payload['sub']:
+            raise AuthError({
+                'code': 'no_permission',
+                'description': 'No permission'
+            }, 401)
+
+        return True
+
+
+    '''
+        @INPUTS
+            token: a json web token for Auth0 with key id (string)
+
+        verifies the token using Auth0 /.well-known/jwks.json
+        decodes the payload from the token
+        validates the claims
+        return the decoded payload
+
+    '''
+
+
+    def verify_decode_jwt(token):
+        jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+        jwks = json.loads(jsonurl.read())
+        unverified_header = jwt.get_unverified_header(token)
+        rsa_key = {}
+        if 'kid' not in unverified_header:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Authorization malformed.'
+            }, 401)
+
+        for key in jwks['keys']:
+            if key['kid'] == unverified_header['kid']:
+                rsa_key = {
+                    'kty': key['kty'],
+                    'kid': key['kid'],
+                    'use': key['use'],
+                    'n': key['n'],
+                    'e': key['e']
+                }
+        if rsa_key:
+            try:
+                payload = jwt.decode(
+                    token,
+                    rsa_key,
+                    algorithms=ALGORITHMS,
+                    audience=API_AUDIENCE,
+                    issuer='https://' + AUTH0_DOMAIN + '/'
+                )
+
+                return payload
+
+            except jwt.ExpiredSignatureError:
+                raise AuthError({
+                    'code': 'token_expired',
+                    'description': 'Token expired.'
+                }, 401)
+
+            except jwt.JWTClaimsError:
+                raise AuthError({
+                    'code': 'invalid_claims',
+                    'description': 'Incorrect claims. \
+                    Please, check the audience and issuer.'
+                }, 401)
+            except Exception:
+                raise AuthError({
+                    'code': 'invalid_header',
+                    'description': 'Unable to parse authentication token.'
+                }, 400)
+        raise AuthError({
+            'code': 'invalid_header',
+                    'description': 'Unable to find the appropriate key.'
+        }, 400)
+
+
+    '''
+        @INPUTS
+            permission: string permission (i.e. 'post:drink')
+
+        uses the get_token_auth_header method to get the token
+        uses the verify_decode_jwt method to decode the jwt
+        uses the check_permissions method validate claims and
+        check the requested permission
+        return the decorator which passes the decoded payload
+        to the decorated method
+    '''
+
+    def requires_auth(permission=''):
+        def requires_auth_decorator(f):
+            @wraps(f)
+            def wrapper(*args, **kwargs):
+
+                token = session['token']['access_token']
+
+                print("requires_auth printing for access token")
+                print(token)
+
+                try:
+                    payload = verify_decode_jwt(token)
+
+                except:
+                    abort(401)
+
+                check_permissions(permission, payload)
+
+                return f(payload, *args, **kwargs)
+
+            return wrapper
+        return requires_auth_decorator
+
+
     def requires_auth_2(f):
         @wraps(f)
         def decorated(*args, **kwargs):
@@ -66,14 +263,9 @@ def create_app(test_config=None):
         # Handles response from token endpoint
         token = auth0.authorize_access_token()
         print(token)
+        print(token['access_token'])
         resp = auth0.get('userinfo')
         userinfo = resp.json()
-
-        print(userinfo)
-
-        resp2 = auth0.get('metadata')
-        metadata = resp.json()
-        print(metadata)
 
         # Store the user information in flask session.
         session['token'] = token
@@ -465,7 +657,10 @@ def create_app(test_config=None):
 
     @app.route('/artists/create', methods=['POST'])
     @requires_auth_2
+    @requires_auth('create:artist')
     def create_artist(payload):
+
+        print(payload)
 
         form = UserForm(request.form)
 
@@ -473,9 +668,15 @@ def create_app(test_config=None):
         user = User.query.filter(User.auth_id==auth_id).one_or_none()
         user_id = user.id
 
+        print("create_artist triggered")
+
         try:
 
+            print("pre validation")
+
             if form.validate():
+
+                print("form validated")
 
                 new_artist = Artist(
                     name = form.artist_name.data,
@@ -485,7 +686,7 @@ def create_app(test_config=None):
 
                 new_artist.insert()
                 flash('Your artist profile has been successfully created.')
-
+            print("form not validated")
         except Exception as e:
 
             print(e)
