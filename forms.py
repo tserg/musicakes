f066dad7926399106537fa37ca364ba24a0f7430
tserg <tse.rong.gary@gmail.com>
2020-06-20 11:30:45 +0800

    feat: validation for username input
    Added validators for username in forms.py.

--- forms.py
+++ forms.py
@@ -1,16 +1,26 @@
+import string
 from datetime import datetime
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField, IntegerField, FloatField
-from wtforms.validators import DataRequired, AnyOf, URL, Optional
+from wtforms.validators import DataRequired, AnyOf, URL, Optional, Length, NoneOf
 
 from werkzeug.utils import secure_filename
 import pycountry
 
+def check_punctuation(form, field):
+	for i in field:
+		if i in string.punctuation:
+			raise ValidationError("Username cannot contain special characters")
+
 class UserForm(FlaskForm):
 
-	username = StringField('username', validators=[DataRequired()])
+	username = StringField('username', validators=[
+		DataRequired(),
+		Length(6, 20, "Username must be between %(min)d to %(max)d characters."),
+		check_punctuation]
+	)
 
 class ArtistForm(FlaskForm):
 
