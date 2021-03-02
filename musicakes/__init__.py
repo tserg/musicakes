aebcb010870b2d7f7c60c0b54c1aed52a992036c
Gary Tse <tse.rong.gary@gmail.com>
2021-03-02 23:27:45 +0800

    fix: flask session not persisting when importing get_user_data

--- musicakes/__init__.py
+++ musicakes/__init__.py
@@ -160,6 +160,48 @@ def create_app(test_config=None):
             'scope': 'openid profile email',
         },
     )
+    ###################################################
+
+    # Routes
+
+    ###################################################
+
+    def get_user_data(return_user_id=False):
+
+        """
+        Helper function to obtain user data and User model object for rendering of page
+        """
+
+        try:
+
+            auth_id = session['jwt_payload']['sub'][6:]
+            user = User.query.filter(User.auth_id==auth_id).one_or_none()
+
+            data = user.short_private()
+
+        except:
+
+            """
+            Key error is thrown if user is not logged in
+            """
+
+            if return_user_id:
+
+                return None, None
+
+            else:
+
+                return None
+
+        else:
+
+            if return_user_id:
+
+                return user, data
+
+            else:
+
+                return data
 
     ###################################################
 
