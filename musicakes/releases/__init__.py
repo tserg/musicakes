fba40f03b56faf1b87941bf133f21a89d47cf744
Gary Tse <tse.rong.gary@gmail.com>
2021-04-29 00:18:17 +0800

    refactor: moved releases js files to blueprint static folder

--- musicakes/releases/__init__.py
+++ musicakes/releases/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 
 bp = Blueprint('releases', __name__,
-    template_folder='templates')
+    template_folder='templates',
+    static_folder='static',
+    static_url_path='/releases/static')
 
 from . import routes
