f37542b4fed9b6595284aedcd57e388691234a95
Gary Tse <tse.rong.gary@gmail.com>
2021-05-07 23:09:54 +0800

    fix: add tasks module to celery config

--- musicakes/config.py
+++ musicakes/config.py
@@ -7,6 +7,7 @@ class CeleryConfig:
     broker_url = os.getenv('REDIS_URL', 'Does not exist')
     result_backend = os.getenv('REDIS_URL', 'Does not exist')
     worker_send_task_events = True
+    include = ['musicakes.tasks']
 
 class FlaskConfig:
     SECRET_KEY = os.getenv('SECRET_KEY', 'Does not exist')
